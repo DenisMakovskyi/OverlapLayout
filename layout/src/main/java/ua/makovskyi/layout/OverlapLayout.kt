95f5abcb6ff2d1e82ef09b8e96269d5b4756af05
Denis Makovskyi <makovsky.denis@gmail.com>
2020-03-17 16:21:35 +0200

    Support animation from left to right and right to left

--- layout/src/main/java/ua/makovskyi/layout/OverlapLayout.kt
+++ layout/src/main/java/ua/makovskyi/layout/OverlapLayout.kt
@@ -2,17 +2,17 @@ package ua.makovskyi.layout
 
 import java.util.concurrent.TimeUnit
 
-import android.animation.Animator
-import android.animation.ValueAnimator
-import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
 import android.os.Parcel
 import android.os.Parcelable
-import android.util.AttributeSet
-import android.view.Gravity
+import android.content.Context
 import android.view.View
+import android.view.Gravity
+import android.graphics.Color
+import android.graphics.Canvas
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.widget.FrameLayout
+import android.util.AttributeSet
 
 import androidx.annotation.AttrRes
 
@@ -65,11 +65,13 @@ open class OverlapLayout(
         }
 
         var isOverlapped = false
+        var overlapWidth = 0
         var overlapHeight = 0
         var remainingTime = 0L
 
         constructor(source: Parcel) : super(source) {
             isOverlapped = source.readByte().toInt() != 0
+            overlapWidth = source.readInt()
             overlapHeight = source.readInt()
             source.readString()?.let {
                 remainingTime = it.toLong()
@@ -81,6 +83,7 @@ open class OverlapLayout(
         override fun writeToParcel(out: Parcel?, flags: Int) {
             super.writeToParcel(out, flags)
             out?.writeByte((if (isOverlapped) 1 else 0).toByte())
+            out?.writeInt(overlapWidth)
             out?.writeInt(overlapHeight)
             out?.writeString(remainingTime.toString())
         }
@@ -90,6 +93,10 @@ open class OverlapLayout(
 
         private const val VIEW_TAG_OVERLAP = "view_overlap"
     }
+
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attributes: AttributeSet?) : this(context, attributes, 0)
 
     var overlapListener: OverlapListener? = null
 
@@ -106,14 +113,11 @@ open class OverlapLayout(
 
     private var isOverlapped = false
     private var isOverlapping = false
+    private var overlapWidth = 0
     private var overlapHeight = 0
     private var remainingTime = 0L
 
     private lateinit var overlapView: View
-
-    constructor(context: Context) : this(context, null, 0)
-
-    constructor(context: Context, attributes: AttributeSet?) : this(context, attributes, 0)
 
     init {
         if (attributes != null) {
@@ -127,7 +131,9 @@ open class OverlapLayout(
                 overlapColor = getColor(R.styleable.OverlapLayout_ol_overlapColor, overlapColor)
                 overlapGravity = when(getInt(R.styleable.OverlapLayout_ol_overlapGravity, overlapGravity)) {
                     0 -> Gravity.TOP
-                    1 -> Gravity.BOTTOM
+                    1 -> Gravity.START
+                    2 -> Gravity.END
+                    3 -> Gravity.BOTTOM
                     else -> throw IllegalArgumentException("Unsupported gravity")
                 }
                 overlapPosition = OverlapPosition.fromInt(getInt(R.styleable.OverlapLayout_ol_overlapPosition, overlapPosition.value))
@@ -151,6 +157,7 @@ open class OverlapLayout(
     override fun onSaveInstanceState(): Parcelable? {
         return SavedState(super.onSaveInstanceState()).also {
             it.isOverlapped = isOverlapped
+            it.overlapWidth = overlapWidth
             it.overlapHeight = overlapHeight
             it.remainingTime = remainingTime
         }
@@ -159,6 +166,7 @@ open class OverlapLayout(
     override fun onRestoreInstanceState(state: Parcelable?) {
         if (state is SavedState) {
             isOverlapped = state.isOverlapped
+            overlapWidth = state.overlapWidth
             overlapHeight = state.overlapHeight
             remainingTime = state.remainingTime
         }
@@ -177,8 +185,18 @@ open class OverlapLayout(
         if(!::overlapView.isInitialized) {
             overlapView = View(context).also {
                 it.tag = VIEW_TAG_OVERLAP
-                it.layoutParams = LayoutParams(measuredWidth, overlapHeight).apply {
-                    gravity = overlapGravity
+                it.layoutParams = when(overlapGravity) {
+                    Gravity.TOP, Gravity.BOTTOM -> {
+                        LayoutParams(measuredWidth, overlapHeight).apply {
+                            gravity = overlapGravity
+                        }
+                    }
+                    Gravity.START, Gravity.END -> {
+                        LayoutParams(overlapWidth, measuredHeight).apply {
+                            gravity = overlapGravity
+                        }
+                    }
+                    else -> throw IllegalArgumentException("Unsupported gravity")
                 }
                 setViewBackground(it, createRectDrawable(overlapColor, cornerRadius))
                 when(overlapPosition) {
@@ -188,7 +206,11 @@ open class OverlapLayout(
             }
 
         } else {
-            overlapView.updateWidth(measuredWidth)
+            when(overlapGravity) {
+                Gravity.TOP, Gravity.BOTTOM -> overlapView.updateWidth(measuredWidth)
+                Gravity.START, Gravity.END -> overlapView.updateHeight(measuredHeight)
+                else -> throw IllegalArgumentException("Unsupported gravity")
+            }
         }
     }
 
@@ -207,44 +229,60 @@ open class OverlapLayout(
 
     private fun animateOverlap(duration: Long) {
         if (!isOverlapped && !isOverlapping) {
-            ValueAnimator.ofInt(overlapView.measuredHeight, measuredHeight).also {
-                it.addUpdateListener { animation ->
-                    overlapHeight = animation.animatedValue as Int
-                    remainingTime = animation.currentPlayTime
-                    overlapView.updateHeight(overlapHeight)
-                }
-                it.addListener(object : AnimationListener() {
-                    override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
-                        super.onAnimationStart(animation, isReverse)
-                        isOverlapped = false
-                        isOverlapping = true
-                        overlapListener?.onOverlapStarted()
-                    }
-
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        isOverlapped = true
-                        isOverlapping = false
-                        overlapListener?.onOverlapCompleted()
-                        evaluateEnabledState()
-                    }
-
-                    override fun onAnimationCancel(animation: Animator?) {
-                        super.onAnimationCancel(animation)
-                        isOverlapped = true
-                        isOverlapping = false
-                        overlapListener?.onOverlapCancelled()
-                        evaluateEnabledState()
-                    }
-                })
-                it.duration = duration
-                it.start()
+            val valueAnimator = when(overlapGravity) {
+                Gravity.TOP, Gravity.BOTTOM -> ValueAnimator.ofInt(overlapView.measuredHeight, measuredHeight)
+                Gravity.START, Gravity.END -> ValueAnimator.ofInt(overlapView.measuredWidth, measuredWidth)
+                else -> throw IllegalArgumentException("Unsupported gravity")
             }
+            valueAnimator.addUpdateListener { animation ->
+                when(overlapGravity) {
+                    Gravity.TOP, Gravity.BOTTOM -> {
+                        overlapHeight = animation.animatedValue as Int
+                        overlapView.updateHeight(overlapHeight)
+                    }
+                    Gravity.START, Gravity.END -> {
+                        overlapWidth = animation.animatedValue as Int
+                        overlapView.updateWidth(overlapWidth)
+                    }
+                    else -> throw IllegalArgumentException("Unsupported gravity")
+                }
+                remainingTime = animation.currentPlayTime
+            }
+            valueAnimator.addListener(object : AnimationListener() {
+                override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                    super.onAnimationStart(animation, isReverse)
+                    isOverlapped = false
+                    isOverlapping = true
+                    overlapListener?.onOverlapStarted()
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    isOverlapped = true
+                    isOverlapping = false
+                    overlapListener?.onOverlapCompleted()
+                    evaluateEnabledState()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    super.onAnimationCancel(animation)
+                    isOverlapped = true
+                    isOverlapping = false
+                    overlapListener?.onOverlapCancelled()
+                    evaluateEnabledState()
+                }
+            })
+            valueAnimator.duration = duration
+            valueAnimator.start()
         }
     }
 
     private fun animatedOverlap() {
-        overlapView.updateHeight(measuredHeight)
+        when(overlapGravity) {
+            Gravity.TOP, Gravity.BOTTOM -> overlapView.updateHeight(measuredHeight)
+            Gravity.START, Gravity.END -> overlapView.updateWidth(measuredWidth)
+            else -> throw IllegalArgumentException("Unsupported gravity")
+        }
         evaluateEnabledState()
     }
 
